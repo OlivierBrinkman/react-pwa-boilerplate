31f4f45c4c28ec7bf2e0b879842b83c9beddf8b7
Olivier  Brinkman <olivier.brinkman@clarify.nl>
2025-01-23 16:45:03 +0100

    update sw name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     if ("serviceWorker" in navigator) {
       window.addEventListener("load", () => {
         navigator.serviceWorker
-          .register("/service-worker.js")
+          .register("/sw.js")
           .then((registration) => {
             console.log(
               "Service Worker registered with scope:",
