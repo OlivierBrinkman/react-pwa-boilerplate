45d232e952a6ea5a7bea1756fd029668389d9a38
Olivier  Brinkman <olivier.brinkman@clarify.nl>
2025-01-23 16:12:23 +0100

    sw updated for push alert

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,26 +1,42 @@
 self.skipWaiting();
 
 self.addEventListener("fetch", function (_event) {
+  console.log("Fetch event triggered");
   //nothing for now
 });
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  console.log("Service worker installing...");
   event.waitUntil(
-      caches.open("your - cache - name").then(cache => {
-          return cache.addAll([
-              "/",
-              "/index.html",
-              "/styles.css",
-              "/script.js"
-          ]);
-      })
+    caches.open("your-cache-name").then((cache) => {
+      console.log("Opened cache");
+      return cache
+        .addAll(["/", "/index.html", "/styles.css", "/script.js"])
+        .then(() => {
+          console.log("All resources have been cached");
+        })
+        .catch((error) => {
+          console.error("Failed to cache resources", error);
+        });
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
+  console.log("Fetch event for ", event.request.url);
   event.respondWith(
-      caches.match(event.request).then(response => {
-          return response || fetch(event.request);
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          console.log("Found ", event.request.url, " in cache");
+          return response;
+        }
+        console.log("Network request for ", event.request.url);
+        return fetch(event.request);
+      })
+      .catch((error) => {
+        console.error("Error fetching data", error);
       })
   );
 });
